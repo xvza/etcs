ee71acda6e98a864d5768dbd2ac964529259a9e8
xvza <>
2020-04-10 02:09:52 +0200

    etcs

--- profile.d/00-environment.sh
+++ profile.d/00-environment.sh
@@ -7,6 +7,7 @@ export    "EDITOR=nano"
 export    "NEED=nature,animals"
 export    "MODE=1920x1080"
 export    "SKEL=/etc/skel"
+export    "IMG=/etc/skel/Picture/_.jpg"
 export    "HTTP=/var/http"
 export    "SHARE=${HTTP}/usrs/local/share"
 ##########################################
