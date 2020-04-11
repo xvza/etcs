a7db921ce3429ddf1398dfec998c637920250091
xvza <>
2020-04-11 13:55:39 +0200

    etcs

--- profile.d/00-environment.sh
+++ profile.d/00-environment.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
-##########################################
+############################################
 export    "SXHKD_SHELL=${SHELL}"
 export    "TMPDIR=/tmp"
 export    "EDITOR=nano"
-##########################################
-export    "NEED=nature,animals"
+############################################
 export    "MODE=1920x1080"
 export    "SKEL=/etc/skel"
-export    "IMG=/etc/skel/Picture/_.jpg"
-export    "HTTP=/var/http"
-export    "SHARE=${HTTP}/usrs/local/share"
-##########################################
-export    "THEMES=/usr/share/themes"
-export    "ICONS=/usr/share/icons"
-##########################################
+export    "SHARE=/var/http/usrs/local/share"
+############################################
