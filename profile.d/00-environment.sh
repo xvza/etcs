dc15ed87f35626bf0d8673a2674c9a52ac70183a
xvza <>
2020-04-12 01:28:58 +0200

    etcs

--- profile.d/00-environment.sh
+++ profile.d/00-environment.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-############################################
+################################
 export    "SXHKD_SHELL=${SHELL}"
 export    "TMPDIR=/tmp"
 export    "EDITOR=nano"
-############################################
+################################
 export    "MODE=1920x1080"
 export    "SKEL=/etc/skel"
-export    "SHARE=/var/http/usrs/local/share"
-############################################
+export    "SHARE=/var/http/shrd"
+################################
