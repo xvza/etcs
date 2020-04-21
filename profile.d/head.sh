6bc2fd11f0fbbc0b99ea9f6dfc4f980825f273ac
xvza <>
2020-04-21 17:38:48 +0200

    etcs

--- profile.d/head.sh
+++ profile.d/head.sh
@@ -3,11 +3,12 @@
 declare   "session=${DESKTOP_SESSION} ${XDG_SESSION_TYPE}"
 declare   "kernel=$(uname -r|cut -d- -f1)"
 declare   "shell=$(basename ${SHELL}) ${BASH_VERSION%(*}"
+declare   "line=seq -s─ $(tput cols)|tr -d [:digit:]"
 ##########################################################
-echo      "------------------------------"
-echo      "term     : ${TERM}"
-echo      "kernel   : ${kernel}"
-echo      "session  : ${session}"
-echo      "shell    : ${shell}"
-echo      "------------------------------"
+eval      "${line}"
+echo      "term     │  ${TERM}"
+echo      "kernel   │  ${kernel}"
+echo      "session  │  ${session}"
+echo      "shell    │  ${shell}"
+eval      "${line}"
 ##########################################################
