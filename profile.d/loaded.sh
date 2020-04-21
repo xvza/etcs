aee3f6d8ef28c40dc30f77e9ea36a10a7fb74eec
xvza <>
2020-04-21 15:02:47 +0200

    etcs

--- profile.d/loaded.sh
+++ profile.d/loaded.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-##########################################
+##########################################################
+declare   "session=${DESKTOP_SESSION} ${XDG_SESSION_TYPE}"
+declare   "kernel=$(uname -r|cut -d- -f1)"
+declare   "shell=$(basename ${SHELL}) ${BASH_VERSION%(*}"
+##########################################################
 echo      "------------------------------"
 echo      "term     : ${TERM}"
-echo      "session  : ${XDG_SESSION_TYPE}"
-echo      "shell    : ${SHELL}"
-echo      "desktop  : ${DESKTOP_SESSION}"
+echo      "kernel   : ${kernel}"
+echo      "session  : ${session}"
+echo      "shell    : ${shell}"
 echo      "------------------------------"
-##########################################
+##########################################################
