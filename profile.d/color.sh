4a729a78d4bea153fae4a7a7595746979e1a2a66
xvza <>
2020-04-28 01:02:41 +0200

    etcs

---
+++ profile.d/color.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#################################
+printf "\e]P02E3440" #black
+printf "\e]P1D08770" #darkred
+printf "\e]P2A3BE8C" #darkgreen
+printf "\e]P3EBCB8B" #brown
+printf "\e]P45E81AC" #darkblue
+printf "\e]P5BF616A" #darkmagenta
+printf "\e]P68FBCBB" #darkcyan
+printf "\e]P7D8DEE9" #lightgrey
+printf "\e]P84C566A" #darkgrey
+printf "\e]P9D08770" #red
+printf "\e]PA8FBCBB" #green
+printf "\e]PBEBCB8B" #yellow
+printf "\e]PC81A1C1" #blue
+printf "\e]PDB48EAD" #magenta
+printf "\e]PE88C0D0" #cyan
+printf "\e]PFECEFF4" #white
+#################################
+clear #for background artifacting
+#################################
