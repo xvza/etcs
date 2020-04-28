eb0e76486217259cd38384b272ddf1e4f0562ed8
xvza <>
2020-04-28 16:32:09 +0200

    etcs

--- profile.d/color.sh
+++ profile.d/color.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-#################################
-printf "\e]P02E3440" #black
-printf "\e]P1D08770" #darkred
-printf "\e]P2A3BE8C" #darkgreen
-printf "\e]P3EBCB8B" #brown
-printf "\e]P45E81AC" #darkblue
-printf "\e]P5BF616A" #darkmagenta
-printf "\e]P68FBCBB" #darkcyan
-printf "\e]P7D8DEE9" #lightgrey
-printf "\e]P84C566A" #darkgrey
-printf "\e]P9D08770" #red
-printf "\e]PA8FBCBB" #green
-printf "\e]PBEBCB8B" #yellow
-printf "\e]PC81A1C1" #blue
-printf "\e]PDB48EAD" #magenta
-printf "\e]PE88C0D0" #cyan
-printf "\e]PFECEFF4" #white
-#################################
-clear #for background artifacting
-#################################
+###############################
+source    "colo.sh"
+###############################
+printf    "\e]P0${black}"
+printf    "\e]P1${darkred}"
+printf    "\e]P2${darkgreen}"
+printf    "\e]P3${brown}"
+printf    "\e]P4${darkblue}"
+printf    "\e]P5${darkmagenta}"
+printf    "\e]P6${darkcyan}"
+printf    "\e]P7${lightgrey}"
+printf    "\e]P8${darkgrey}"
+printf    "\e]P9${red}"
+printf    "\e]PA${green}"
+printf    "\e]PB${yellow}"
+printf    "\e]PC${blue}"
+printf    "\e]PD${magenta}"
+printf    "\e]PE${cyan}"
+printf    "\e]PF${white}"
+###############################
