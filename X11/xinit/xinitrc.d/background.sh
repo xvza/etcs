d5953a60fa96756f144811ebe0b288b4213b7745
xvza <>
2020-05-06 13:36:04 +0200

    etcs

--- X11/xinit/xinitrc.d/background.sh
+++ X11/xinit/xinitrc.d/background.sh
@@ -1,15 +1,1 @@
-#!/usr/bin/env bash
-###########################################################
-set -u
-###########################################################
-declare   "arg=${WIDTH}x${HEIGHT}?nature,animals"
-declare   "url=https://source.unsplash.com/${arg}"
-declare   "path=org.gnome.desktop.background"
-declare   "img=/etc/skel/Picture/_.jpg"
-declare   "file=file://${img}"
-###########################################################
-eval      "display -window root ${file}"
-eval      "gsettings set ${path} picture-uri ${file}"
-###########################################################
-source    "net2.sh"  "${img}"  "${url}"  "${img}"
-###########################################################
+/var/http/srcs/autostart.d/background.sh
